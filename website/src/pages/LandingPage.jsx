ecace05008e1a212daf599494199fea4328319e9
agnes <a.tjokrosetio@student.unsw.edu.au>
2025-01-31 02:07:00 +1100

    update landing page description

--- website/src/pages/LandingPage.jsx
+++ website/src/pages/LandingPage.jsx
@@ -26,7 +26,7 @@ function LandingPage() {
           <h1 className="text-[1.5rem] font-bold text-white">Hi, I'm Agnes!</h1>
           <p className="text-[1rem] text-white">
             I am a 3rd year UNSW student studying a <b>Bachelor of Computer Science / Commerce</b>, majoring in Computer
-            Science and Finance. I have a strong interest in{" "}
+            Science and Finance. My career aspirations lie mainly in Computer Science and I have a strong interest in{" "}
             <b>software product development, software engineering and frontend developing and engineering</b>.
           </p>
           <p className="text-[1rem] text-white">

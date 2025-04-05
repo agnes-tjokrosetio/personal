acf2f1392031febbfda73f8a14f343d875660fb7
agnes <a.tjokrosetio@student.unsw.edu.au>
2025-04-05 18:27:20 +1100

    changed routers

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import AboutMePage from "./pages/AboutMePage";
 import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   return (
-    <BrowserRouter basename="/personal">
+    <HashRouter basename="/personal">
       <Routes>
         {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
@@ -15,7 +15,7 @@ function App() {
         {/* Projects Page */}
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

31efe2a09405bb8bbb975086689b6a5bdc91bf60
agnes <a.tjokrosetio@student.unsw.edu.au>
2025-05-19 19:22:25 +1000

    update

--- website/src/components/layout/ContactInformation.jsx
+++ website/src/components/layout/ContactInformation.jsx
@@ -1,13 +1,13 @@
 function ContactInformation() {
   return (
-    <div className="h-auto py-[5vh] px-[5vw] flex flex-row justify-center text-left gap-2">
+    <div className="h-auto py-[5vh] px-[5vw] flex flex-row justify-center text-left gap-6">
       <a
         className="!text-white hover:text-gray-400 transition duration-300"
         href="https://www.linkedin.com/in/agnestjokrosetio/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fa fa-linkedin" aria-hidden="true"></i>
+        <i className="fa fa-linkedin !text-2xl" aria-hidden="true"></i>
       </a>
       <a
         className="!text-white hover:text-gray-400 transition duration-300"
@@ -15,7 +15,7 @@ function ContactInformation() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fa fa-github" aria-hidden="true"></i>
+        <i className="fa fa-github !text-2xl" aria-hidden="true"></i>
       </a>
       <a
         className="!text-white hover:text-gray-400 transition duration-300"
@@ -23,7 +23,7 @@ function ContactInformation() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fa fa-envelope" aria-hidden="true"></i>
+        <i className="fa fa-envelope !text-2xl" aria-hidden="true"></i>
       </a>
     </div>
   );
